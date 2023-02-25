c960ea8236fa9b4356b97c34210466af7f7d7e79
Awaizkazi <Owaizkazi789@gmail.com>
2023-02-25 12:24:38 +0530

    initial commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -5,6 +5,36 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffcef0ff),
+        body: Center(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 40.0),
+                child: Text(
+                  'Online Course',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Text(
+                'Noursish your\n    inner skills',
+                style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Choose your favorite course\n       and start learning',
+                style: TextStyle(
+                  height: 2.2,
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
